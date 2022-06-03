167062b4ab982dde2fe99cc60881bf167ea48b70
Henok-Haile <henokhaile53@gmail.com>
2022-06-03 16:13:33 -0700

    Task_3

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints alpahabet in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+        int ch;
+	ch = 'a'
+
+	while (ch <= 'z')
+	if ch == 'e' || 'q'
+	putchar(ch);
+	putchar('\n');
+	return (0);
+}
+
