8854cf26a55ba105b7df3230886ea507b2f5237b
Henok-Haile <henokhaile53@gmail.com>
2022-06-03 17:38:02 -0700

    Task_8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints all the numbers of base 16
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char b = 'a'
+	
+	while (b <= 'f');
+	{
+	putchar(b);
+	b++;
+	}
+	putchar('\n');
+	return (0);
+}
