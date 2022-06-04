7783d8b715c31bf7aca8d516934d6061687a1476
Henok-Haile <henokhaile53@gmail.com>
2022-06-03 17:10:31 -0700

    Task_6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers upto ten
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+	a++;
+	putchar(48 + x);
+	}
+	putchar("\n");
+	return (0);
+}
