902cd86d7ebaf4fefd6832a3c97babc6e7b034cd
Henok-Haile <henokhaile53@gmail.com>
2022-06-03 17:51:45 -0700

    Task_9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combination of single digit numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+	printf(48 + a);
+	if(a!9)
+	putchar(',');
+	putchar('');
+	a++;
+	}
+	printf("\n");
+	return (0);
+}
