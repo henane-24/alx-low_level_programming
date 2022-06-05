259e1a818fa86a75088b0c0fffeebfc33c862e53
Henok-Haile <henokhaile53@gmail.com>
2022-06-05 05:06:20 -0700

    Task_10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,19 +7,23 @@
  */
 int main(void)
 {
-	int i, j;
-	for (i = 48; i <= 56; i++)
-		for (j = 49; j <= 57; j++)
-			if (i < j>)
-			{
-				putchar(i);
-				putchar(j);
-			}
-			if ((i < 56) || (j < 57))
-			{
-				putchar(44);
-				putchar(32);
-			}
-			putchar(10);
-			return (0);
+int i, j;
+for (i = 48; i <= 56; i++)
+{
+for (j = 49; j <= 57; j++)
+{
+if (i < j)
+{
+putchar(i);
+putchar(j);
+if ((i < 56) || (j < 57))
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+}
+putchar(10);
+return (0);
 }
