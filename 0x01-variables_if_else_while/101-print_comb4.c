bfa8ecc4109d6c86b24be273b035920f60627678
Henok-Haile <henokhaile53@gmail.com>
2022-06-05 05:21:20 -0700

    Task_11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints all possible differnet combinations of three digits
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, k;
+	for (i = 48; i <= 55; i++)
+	{
+		for (j = 49; j <= 56; j++)
+		{
+			for (k = 50; k <= 57)
+			{
+				if (i < j && i < k && j < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if ((i < 55) || (j < 56) || (k < 57))
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
+
