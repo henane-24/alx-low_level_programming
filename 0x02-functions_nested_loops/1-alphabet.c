1b46905c9cc552c1210e82b82214d8e4b668050f
Henok-Haile <henokhaile53@gmail.com>
2022-06-07 14:14:06 -0700

    Task_1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,17 @@
  * Return: Always 0.
  */
 void print_alphabet(void)
+{
+	char alpahbet = 'a';
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
 
 int main(void)
 {
     print_alphabet();
-    _putchar(print_alphabet);
     _putchar('\n');
     return (0);
 }
