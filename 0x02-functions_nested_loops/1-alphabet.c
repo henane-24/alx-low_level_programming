4d6424953879d6173051dae50971241742a4441e
Henok-Haile <henokhaile53@gmail.com>
2022-06-07 14:17:42 -0700

    Task_1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include "main.h"
 
 /**
@@ -10,7 +11,7 @@ void print_alphabet(void)
 	char alpahbet = 'a';
 	while (alphabet <= 'z')
 	{
-		putchar(alphabet);
+		_putchar(alphabet);
 		alphabet++;
 	}
 
