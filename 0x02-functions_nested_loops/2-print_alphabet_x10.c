77929c412e2dbefd62d1b7f645af6f6cb0ffdb8b
Henok-Haile <henokhaile53@gmail.com>
2022-06-07 15:56:53 -0700

    Task_2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - check the code.
- *
+ * print_alphabet - check the code.
+ * 
  * Return: Always 0.
  */
 void print_alphabet_x10(void)
@@ -10,12 +10,12 @@ void print_alphabet_x10(void)
 	char alphabet;
 	int i;
 	
-		for (i = 0; i <= 10; i++)
+	for (i = 0; i <= 9; i++)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		{
-			for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-			{
-			_putchar(alphabet);
-			}
-			_putchar('\n');
+		_putchar(alphabet);
 		}
+		_putchar('\n');
+	}
 }
