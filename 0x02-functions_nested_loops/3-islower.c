945a2bafe46fe560c25ec3ba06a6381d0e7abe5d
Henok-Haile <henokhaile53@gmail.com>
2022-06-07 16:15:05 -0700

    Task_3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * _islower - check the code.
  *
  * Return: Always 0.
  */
 int _islower(int c)
 {
-	if (c == islower)
+	if (c == _islower)
 	{
 		return (1);
 	}
