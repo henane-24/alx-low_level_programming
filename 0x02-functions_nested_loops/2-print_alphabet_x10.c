0add3f3ed0c2434f1ade8ef843a4037d597d1dc9
Henok-Haile <henokhaile53@gmail.com>
2022-06-07 15:51:23 -0700

    Task_2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,12 +10,12 @@ void print_alphabet_x10(void)
 	char alphabet;
 	int i;
 	
-		for (i = 0; i <= 10, i++)
+		for (i = 0; i <= 10; i++)
 		{
-			for (alphabet = 'a', alphabet <= 'z' ; alphabet++)
+			for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 			{
 			_putchar(alphabet);
 			}
-			_putchar('\n)
+			_putchar('\n);
 		}
 }
