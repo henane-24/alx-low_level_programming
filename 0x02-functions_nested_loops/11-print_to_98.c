7a1f5262437bd37fdeceefdd8e73be9b36e8a169
Henok-Haile <henokhaile53@gmail.com>
2022-06-07 18:27:00 -0700

    Task_11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,13 +8,13 @@ void print_to_98(int n)
 {
 	if (n >= 98)
 	{
-		while (n >= 98) 
+		while (n > 98) 
 			printf("%d, ", n--);
-		printf("5d\n", n);
+		printf("%d\n", n);
 	}
 	else
 	{
-		whike (n < 98)
+		wlike (n < 98)
 			printf("%d, ", n++);
 		printf("%d\n", n);
 	}
