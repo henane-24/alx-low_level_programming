f15680038dfb121389df144c4e0df5f05775ba99
Henok-Haile <henokhaile53@gmail.com>
2022-06-07 17:36:43 -0700

    Task_6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - check the code
+ * @n: the integer to be computed
+ * Return: Always 0.
+ */
+int _abs(int)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
