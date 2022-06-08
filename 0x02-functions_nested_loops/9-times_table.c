1828a2e78f2dd7c635b10a03fbd779d62777d2c1
Henok-Haile <henokhaile53@gmail.com>
2022-06-07 18:13:18 -0700

    Task_9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,6 +6,7 @@
 void times_table(void)
 {
 	int num, mult, prod;
+
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
@@ -14,9 +15,9 @@ void times_table(void)
 		{
 			_putchar(',');
 			_putchar(' ');
-
-			prod = num * mult;
 			
+			prod = num * mult;
+
 			if (prod <= 9)
 			{
 				_putchar(' ');
