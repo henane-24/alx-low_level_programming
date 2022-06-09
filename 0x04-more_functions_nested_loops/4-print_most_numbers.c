fdc22985bb79ab97a47e09d4622872229c991ade
Henok-Haile <henokhaile53@gmail.com>
2022-06-09 10:49:14 -0700

    Task_4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,12 +9,14 @@
 
 void print_most_numbers(void)
 {
-	int c;
+int c;
 
-	for (c = 48; c < 58; c++)
-	{
-		if (c != 50 && c != 52)
-			_putchar(c);
-	}
-	_putchar('\n');
+for (c = 48; c < 58; c++)
+{
+if (c != 50 && c != 52)
+{
+_putchar(c);
+}
+}
+_putchar('\n');
 }
