f2a8c8acb83f92e19fcbf2625412a60d5ebf708a
Henok-Haile <henokhaile53@gmail.com>
2022-06-09 10:37:58 -0700

    Task_5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+# include "main.h"
+
+/**
+ * more_numbers - A function that prints 10 times the numbers
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i;
+	i = 0;
+	
+	while (i < 10)
+	{
+		j = 0;
+
+		while (j < 15)
+		{
+			if (j >= 10)
+				_putchar((j / 10) + 48);
+			_putchar((j % 10) + 48)
+			j++;
+
+		}
+		_putchar('\n');
+		i++;
+	}
+}
+
