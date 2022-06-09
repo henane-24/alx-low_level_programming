ce75df39859d5c1b5be5ba112d7879c554c1f078
Henok-Haile <henokhaile53@gmail.com>
2022-06-09 08:24:05 -0700

    Task_4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints numbers 1 upto 9 except 2 and 4
+ * print_most_numbers - prints numbers 1 upto 9
+ * except 2 and 4
  * @c: integer
  * Return: void
  */
