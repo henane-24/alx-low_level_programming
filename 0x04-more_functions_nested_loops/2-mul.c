e244259058301cced3bfb6a3d24eefb465fc6d5d
Henok-Haile <henokhaile53@gmail.com>
2022-06-09 06:27:14 -0700

    Task_2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplication of two integers
+ * @a: input integer
+ * @b: input integer
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	char prod;
+	prod = a * b;
+	return (0);
+}
