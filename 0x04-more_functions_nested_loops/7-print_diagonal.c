0d1ff9039721788b21be8e19b36d5941a944b85e
Henok-Haile <henokhaile53@gmail.com>
2022-06-09 11:11:34 -0700

    Task_7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_diagonal - A function that drwas line on the terminal
+ * @n: The number of times the char \ to be printed
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i =0; i < n: i++)
+	{
+		if (n <= 0)
+			_putchar('\n');
+		_putchar(92);
+	}
+	_putchar('\n');
+}
