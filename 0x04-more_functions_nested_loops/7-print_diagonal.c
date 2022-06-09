f5db39e96dbb761cc783de5d88e995a1e2227588
Henok-Haile <henokhaile53@gmail.com>
2022-06-09 11:22:40 -0700

    Task_7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,13 +8,18 @@
 
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
 	for (i =0; i < n; i++)
 	{
+		for (j = 0; j < i; j++)
+		{
+		_putchar(32);
+		}
+		_putchar(92);
+
 		if (n <= 0)
 			_putchar('\n');
-		_putchar(92);
 	}
 	_putchar('\n');
 }
