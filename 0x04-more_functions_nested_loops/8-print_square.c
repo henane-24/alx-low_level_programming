9b60fcaebe1775e1ec110df3f510a6e4a0bee2e6
Henok-Haile <henokhaile53@gmail.com>
2022-06-10 05:10:00 -0700

    Task_8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,18 +8,15 @@
 
 void print_square(int size)
 {
-int i, j;
-	
+int i;
+
 for (i = 0; i < size; i++)
 {
-for (j =0; j < size; j++)
-{
-_putchar(35);
-}
-if (i != size - 1)
+if (n <= 0)
 {
 _putchar('\n');
 }
+_putchar(35);
 }
 _putchar('\n');
 }
