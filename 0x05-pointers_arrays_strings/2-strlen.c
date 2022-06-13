d3d44afcfd0916fcf84b3bf5bd4a85dd6c040ad6
Henok-Haile <henokhaile53@gmail.com>
2022-06-13 12:33:47 -0700

    Task_2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * int _strlen - A function that returns the length of a string
+ * @s: input string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	count = 0;
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	return (count);
+}
