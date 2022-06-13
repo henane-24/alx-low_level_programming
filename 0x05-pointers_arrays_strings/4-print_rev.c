88a6f84a6d4f52b5b44733681c4eb68fc5a48d27
Henok-Haile <henokhaile53@gmail.com>
2022-06-13 12:52:08 -0700

    Task_4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - A function that prints a string, in revers
+ * with a new line
+ * @s: a string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+	{
+		count++
+	}
+	count = count - 1;
+		while(count >= 0)
+		{
+			_putchar(s[count]);
+			count--;
+		}
+	_putchar('\n');
+}
