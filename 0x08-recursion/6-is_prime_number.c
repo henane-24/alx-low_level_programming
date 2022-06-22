875229d3b170863cee7645d6004c9024e07e6eab
Henok-Haile <henokhaile53@gmail.com>
2022-06-22 08:27:40 -0700

    Task_6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * is_prime_num - helper function
- * @n: number
- * @x: number
+ * @n: num
+ * @x: num
  * Return: output
  */
 int is_prime_num(int n, int x)
@@ -16,4 +16,14 @@ int is_prime_num(int n, int x)
 		return (1);
 	}
 	return (is_prime_num(n, x + 1));
+}
+/**
+ * is_prime_number - function that returns 1 if the input integer
+ * is a prime number, otherwise return 0
+ * @n: num
+ * Return: output
+ */
+int is_prime_number(int n)
+{
+	return (is_prime_num(n, 2));
 }
