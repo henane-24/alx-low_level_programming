f840bcd58d44ff5d62030e263b6725bbf2aafbae
Henok-Haile <henokhaile53@gmail.com>
2022-07-21 15:50:19 -0700

    Task_6

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,13 +2,12 @@
 
 /**
 * get_endianness - checks the endianness
-*
 * Return: 0 if big endian, 1 if little endian
 */
 
 int get_endianness(void)
 {
-	
+
 	unsigned int i = 1;
 	char *c = (char *) &i;
 
